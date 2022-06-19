5cd738c67b8fea52e5f656f83fbcdcd37501f6ed
Junhong Deng <dengjunhong@gmail.com>
2022-06-19 03:57:50 -0700

    testing

--- app/test_kernel.cpp
+++ app/test_kernel.cpp
@@ -34,6 +34,7 @@ TEMPLATE_TEST_CASE("orientation", "[Kernel]", float, double) {
     typename Kernel::Statistics stats;
     Kernel::clear_statistics();
     Kernel::get_statistics(stats);
+    Kernel k;
 
     SECTION("orientation with interval") {
         typename Kernel::Point a(TestType(0), TestType(0));
@@ -42,9 +43,9 @@ TEMPLATE_TEST_CASE("orientation", "[Kernel]", float, double) {
         typename Kernel::Point d(TestType(1), TestType(1));
         typename Kernel::Point e(TestType(0), TestType(2));
 
-        CHECK(Kernel::orientation(a, b, c) == Kernel::Orientation::right_turn);
-        CHECK(Kernel::orientation(a, b, d) == Kernel::Orientation::collinear);
-        CHECK(Kernel::orientation(a, b, e) == Kernel::Orientation::left_turn);
+        CHECK(k.orientation(a, b, c) == Kernel::Orientation::right_turn);
+        CHECK(k.orientation(a, b, d) == Kernel::Orientation::collinear);
+        CHECK(k.orientation(a, b, e) == Kernel::Orientation::left_turn);
 
         CHECK(stats.orientation_total_count == std::size_t(3));
         CHECK(stats.orientation_exact_count == std::size_t(0));
@@ -56,8 +57,8 @@ TEMPLATE_TEST_CASE("orientation", "[Kernel]", float, double) {
         typename Kernel::Point c(TestType(1.0000000000000000001), TestType(1));
         typename Kernel::Point d(TestType(0.9999999999999999999), TestType(1));
 
-        CHECK(Kernel::orientation(a, b, c) == Kernel::Orientation::right_turn);
-        CHECK(Kernel::orientation(a, b, d) == Kernel::Orientation::left_turn);
+        CHECK(k.orientation(a, b, c) == Kernel::Orientation::right_turn);
+        CHECK(k.orientation(a, b, d) == Kernel::Orientation::left_turn);
 
         CHECK(stats.orientation_total_count == std::size_t(5));
         CHECK(stats.orientation_exact_count == std::size_t(2));
@@ -71,6 +72,7 @@ TEMPLATE_TEST_CASE("side_of_oriented_circle", "[Kernel]", float, double) {
     typename Kernel::Statistics stats;
     Kernel::clear_statistics();
     Kernel::get_statistics(stats);
+    Kernel k;
 
     SECTION("side_of_oriented_circle with interval") {
         typename Kernel::Point a(TestType(0), TestType(0));
@@ -80,9 +82,9 @@ TEMPLATE_TEST_CASE("side_of_oriented_circle", "[Kernel]", float, double) {
         typename Kernel::Point e(TestType(2), TestType(2));
         typename Kernel::Point f(TestType(3), TestType(3));
 
-        CHECK(Kernel::side_of_oriented_circle(a, b, c, d) == Kernel::Oriented_side::on_positive_side);
-        CHECK(Kernel::side_of_oriented_circle(a, b, c, e) == Kernel::Oriented_side::on_boundary);
-        CHECK(Kernel::side_of_oriented_circle(a, b, c, f) == Kernel::Oriented_side::on_negative_side);
+        CHECK(k.side_of_oriented_circle(a, b, c, d) == Kernel::Oriented_side::on_positive_side);
+        CHECK(k.side_of_oriented_circle(a, b, c, e) == Kernel::Oriented_side::on_boundary);
+        CHECK(k.side_of_oriented_circle(a, b, c, f) == Kernel::Oriented_side::on_negative_side);
 
         CHECK(stats.side_of_oriented_circle_total_count == std::size_t(3));
         CHECK(stats.side_of_oriented_circle_exact_count == std::size_t(0));
@@ -95,8 +97,8 @@ TEMPLATE_TEST_CASE("side_of_oriented_circle", "[Kernel]", float, double) {
         typename Kernel::Point d(TestType(1.9999999999999999999), TestType(2));
         typename Kernel::Point e(TestType(2.0000000000000000001), TestType(2));
 
-        CHECK(Kernel::side_of_oriented_circle(a, b, c, d) == Kernel::Oriented_side::on_positive_side);
-        CHECK(Kernel::side_of_oriented_circle(a, b, c, e) == Kernel::Oriented_side::on_negative_side);
+        CHECK(k.side_of_oriented_circle(a, b, c, d) == Kernel::Oriented_side::on_positive_side);
+        CHECK(k.side_of_oriented_circle(a, b, c, e) == Kernel::Oriented_side::on_negative_side);
 
         CHECK(stats.side_of_oriented_circle_total_count == std::size_t(5));
         CHECK(stats.side_of_oriented_circle_exact_count == std::size_t(2));
@@ -110,6 +112,7 @@ TEMPLATE_TEST_CASE("preferred_direction", "[Kernel]", float, double) {
     typename Kernel::Statistics stats;
     Kernel::clear_statistics();
     Kernel::get_statistics(stats);
+    Kernel k;
 
     SECTION("preferred_direction with interval") {
         typename Kernel::Point a(TestType(0), TestType(0));
@@ -120,9 +123,9 @@ TEMPLATE_TEST_CASE("preferred_direction", "[Kernel]", float, double) {
         typename Kernel::Vector v(TestType(1), TestType(0));
         typename Kernel::Vector w(TestType(-1), TestType(2));
 
-        CHECK(Kernel::preferred_direction(a, b, c, u) == int(1));
-        CHECK(Kernel::preferred_direction(a, b, c, v) == int(0));
-        CHECK(Kernel::preferred_direction(a, b, c, w) == int(-1));
+        CHECK(k.preferred_direction(a, b, c, u) == int(1));
+        CHECK(k.preferred_direction(a, b, c, v) == int(0));
+        CHECK(k.preferred_direction(a, b, c, w) == int(-1));
 
         CHECK(stats.preferred_direction_total_count == std::size_t(3));
         CHECK(stats.preferred_direction_exact_count == std::size_t(0));
@@ -136,8 +139,8 @@ TEMPLATE_TEST_CASE("preferred_direction", "[Kernel]", float, double) {
         typename Kernel::Vector u(TestType(1), TestType(0.0000000000000000001));
         typename Kernel::Vector v(TestType(1), TestType(-0.0000000000000000001));
 
-        CHECK(Kernel::preferred_direction(a, b, c, u) == int(1));
-        CHECK(Kernel::preferred_direction(a, b, c, v) == int(-1));
+        CHECK(k.preferred_direction(a, b, c, u) == int(1));
+        CHECK(k.preferred_direction(a, b, c, v) == int(-1));
 
         CHECK(stats.preferred_direction_total_count == std::size_t(5));
         CHECK(stats.preferred_direction_exact_count == std::size_t(2));
@@ -151,6 +154,7 @@ TEMPLATE_TEST_CASE("is_strictly_convex_quadrilateral", "[Kernel]", float, double
     typename Kernel::Statistics stats;
     Kernel::clear_statistics();
     Kernel::get_statistics(stats);
+    Kernel k;
 
     SECTION("is_strictly_convex_quadrilateral with interval") {
         typename Kernel::Point a(TestType(0), TestType(0));
@@ -160,9 +164,9 @@ TEMPLATE_TEST_CASE("is_strictly_convex_quadrilateral", "[Kernel]", float, double
         typename Kernel::Point e(TestType(1.5), TestType(1));
         typename Kernel::Point f(TestType(0.3), TestType(0.3));
 
-        CHECK(Kernel::is_strictly_convex_quad(a, b, c, d) == true);
-        CHECK(Kernel::is_strictly_convex_quad(a, b, c, e) == false);
-        CHECK(Kernel::is_strictly_convex_quad(a, b, c, f) == false);
+        CHECK(k.is_strictly_convex_quad(a, b, c, d) == true);
+        CHECK(k.is_strictly_convex_quad(a, b, c, e) == false);
+        CHECK(k.is_strictly_convex_quad(a, b, c, f) == false);
 
         CHECK(stats.orientation_total_count == std::size_t(3));
         CHECK(stats.orientation_exact_count == std::size_t(0));
@@ -175,8 +179,8 @@ TEMPLATE_TEST_CASE("is_strictly_convex_quadrilateral", "[Kernel]", float, double
         typename Kernel::Point d(TestType(1.5000000000000000001), TestType(1.5));
         typename Kernel::Point e(TestType(1.4999999999999999999), TestType(1.5));
 
-        CHECK(Kernel::is_strictly_convex_quad(a, b, c, d) == true);
-        CHECK(Kernel::is_strictly_convex_quad(a, b, c, e) == false);
+        CHECK(k.is_strictly_convex_quad(a, b, c, d) == true);
+        CHECK(k.is_strictly_convex_quad(a, b, c, e) == false);
 
         CHECK(stats.orientation_total_count == std::size_t(5));
         CHECK(stats.orientation_exact_count == std::size_t(2));
@@ -190,6 +194,7 @@ TEMPLATE_TEST_CASE("is_locally_delaunay_edge", "[Kernel]", float, double) {
     typename Kernel::Statistics stats;
     Kernel::clear_statistics();
     Kernel::get_statistics(stats);
+    Kernel k;
 
     SECTION("is_locally_delaunay_edge with interval") {
         typename Kernel::Point a(TestType(0), TestType(0));
@@ -199,9 +204,9 @@ TEMPLATE_TEST_CASE("is_locally_delaunay_edge", "[Kernel]", float, double) {
         typename Kernel::Point e(TestType(2), TestType(2));
         typename Kernel::Point f(TestType(3), TestType(3));
 
-        CHECK(Kernel::is_locally_delaunay_edge(a, b, c, d) == false);
-        CHECK(Kernel::is_locally_delaunay_edge(a, b, c, e) == true);
-        CHECK(Kernel::is_locally_delaunay_edge(a, b, c, f) == true);
+        CHECK(k.is_locally_delaunay_edge(a, b, c, d) == false);
+        CHECK(k.is_locally_delaunay_edge(a, b, c, e) == true);
+        CHECK(k.is_locally_delaunay_edge(a, b, c, f) == true);
 
         CHECK(stats.orientation_total_count == std::size_t(3));
         CHECK(stats.orientation_exact_count == std::size_t(0));
@@ -214,8 +219,8 @@ TEMPLATE_TEST_CASE("is_locally_delaunay_edge", "[Kernel]", float, double) {
         typename Kernel::Point d(TestType(1.9999999999999999999), TestType(2));
         typename Kernel::Point e(TestType(2.0000000000000000001), TestType(2));
 
-        CHECK(Kernel::is_locally_delaunay_edge(a, b, c, d) == false);
-        CHECK(Kernel::is_locally_delaunay_edge(a, b, c, e) == true);
+        CHECK(k.is_locally_delaunay_edge(a, b, c, d) == false);
+        CHECK(k.is_locally_delaunay_edge(a, b, c, e) == true);
 
         CHECK(stats.side_of_oriented_circle_total_count == std::size_t(5));
         CHECK(stats.side_of_oriented_circle_exact_count == std::size_t(2));
